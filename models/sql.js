2d1ffd97d38fa0ed7cb0d71b39411e58cbb935db
althea1224 <1451909684@qq.com>
2019-11-27 14:48:41 +0800

    remove

--- models/sql.js
+++
@@ -1,9 +0,0 @@
-//SQL语句封装
-const SQLURL = {
-    getBankList: 'SELECT * FROM `shoubaodan`.`sys_bank`',
-    getBankById: 'SELECT * FROM `shoubaodan`.`sys_bank` where id = ?',
-    getUserInfo: 'SELECT * FROM `shoubaodan`.`user_info` where user_account = ?',
-    updateUserInfo: 'UPDATE `shoubaodan`.`user_info` SET user_name=? WHERE user_id=?',
-}
-
-module.exports = SQLURL;
