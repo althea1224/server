660e91712139c06d510c4b8479eef4893616d67c
althea1224 <1451909684@qq.com>
2019-11-27 14:57:38 +0800

    sss

--- models/index.js
+++ models/index.js
@@ -9,7 +9,7 @@
 //引入mysql
 const mysql = require('mysql');
 //引入mysql连接配置
-const mysqlconfig = require('../config/mysql');
+const mysqlconfig = require('./mysql');
 //引入连接池配置
 const poolextend = require('./poolextend')
 //使用连接池，提升性能
---
+++ models/mysql.js
@@ -0,0 +1,10 @@
+// MySQL数据库联接配置封裝
+var mysql = {
+    host: 'rm-uf6l3gla1k92g9pw8.mysql.rds.aliyuncs.com',
+    user: 'hauto2019',
+    password: 'Hauto@123',
+    database: 'shoubaodan',
+    port: 3306,
+    connectionLimit: 0
+};
+module.exports = mysql;
